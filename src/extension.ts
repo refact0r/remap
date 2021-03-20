10d336f109e93a9d5146212e2e0c722c83b24e8d
FatBanana <34758569+refact0r@users.noreply.github.com>
2021-03-19 17:07:04 -0700

    release 0.0.2

--- src/extension.ts
+++ src/extension.ts
@@ -22,6 +22,74 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('remap.toggleSelectMode', () => {
 		app.toggleSelectMode();
 	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.toggleDeleteMode', () => { 
+		app.toggleDeleteMode(); 
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.up', () => {
+		app.up();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.left', () => {
+		app.left();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.down', () => { 
+		app.down(); 
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.right', () => {
+		app.right();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.leftWord', () => {
+		app.leftWord();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.rightWord', () => { 
+		app.rightWord(); 
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.lineStart', () => {
+		app.lineStart();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.lineEnd', () => {
+		app.lineEnd();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.newLine', () => { 
+		app.newLine(); 
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.undo', () => {
+		app.undo();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.redo', () => {
+		app.redo();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.copy', () => { 
+		app.copy(); 
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.paste', () => {
+		app.paste();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.cut', () => {
+		app.cut();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.outdent', () => { 
+		app.outdent(); 
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('remap.indent', () => { 
+		app.indent(); 
+	}));
 }
 
 export function deactivate() {
